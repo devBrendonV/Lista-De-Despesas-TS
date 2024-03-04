cb325c21ba4252aced3605f8cecf25afcc9eaf9a
Brendon Victor <devbrendonvs@gmail.com>
2024-03-04 10:04:24 -0300

    /Context Estados do componente criados

--- src/Context/index.tsx
+++ src/Context/index.tsx
@@ -1,6 +1,14 @@
-import { createContext, ReactNode } from "react";
+import { createContext, useState,ReactNode } from "react";
+import { FormatoLista } from "../types/FormatoLista";
 
-interface ContextProps {}
+
+interface ContextProps {
+  total : number,
+  entrada: number,
+  saida: number,
+  listaGastos: FormatoLista[],
+  tipoTransacao: boolean, 
+}
 
 export const Context = createContext<ContextProps | undefined>(undefined);
 
@@ -11,5 +19,14 @@ interface ContextProviderProps {
 export const ContextProvider: React.FC<ContextProviderProps> = ({
   children,
 }) => {
-  return <Context.Provider value={{}}>{children}</Context.Provider>;
+  const [total, setTotal] = useState<number>(0);
+  const [entrada, setEntrada] = useState<number>(0);
+  const [saida, setSaida] = useState<number>(0);
+  const [listaGastos, setListaGastos] = useState<FormatoLista[]>([]);
+  const [tipoTransacao, setTipoTransacao] = useState(true);
+
+
+
+
+  return <Context.Provider value={{total,entrada,saida,listaGastos,tipoTransacao}}>{children}</Context.Provider>;
 };
