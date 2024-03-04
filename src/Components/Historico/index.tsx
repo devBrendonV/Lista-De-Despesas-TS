c80dd6af978a7aec80eb5322dc76949b05d7f9ac
Brendon Victor <devbrendonvs@gmail.com>
2024-03-04 11:31:06 -0300

    Components/Historico configurado

--- src/Components/Historico/index.tsx
+++ src/Components/Historico/index.tsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import { Context } from "../../Context";
+import { Box } from "@mui/material";
+import { ItemLista } from "./ItemLista";
+import { useContext } from "react";
 
 export const Historico = () => {
+  const { listaGastos } = useContext(Context);
+
+  if (listaGastos.length === 0) {
+    return (
+      <Box
+        display={"flex"}
+        justifyContent={"center"}
+        margin={"10px 0px 0px 0"}
+        alignItems={"center"}
+        color={"black"}
+        fontSize={18}
+        height={"250px"}
+      >
+        Lista Vazia
+      </Box>
+    );
+  }
+
   return (
-    <div>Historico</div>
-  )
-}
+    <Box height={"250px"}>
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"flex-start"}
+        margin={"10px 0px 0px 0"}
+        paddingRight={"5px"}
+        alignItems={"center"}
+        color={"black"}
+        fontSize={18}
+        height={"250px"}
+        overflow={"auto"}
+        boxSizing={"border-box"}
+      >
+        {listaGastos.map((e, i) => {
+          return (
+            <ItemLista
+              key={i}
+              value={{
+                transacao: e.nome,
+                tipo: e.tipo,
+                valor: e.valor,
+                indice: i,
+                conteudo: e,
+              }}
+            ></ItemLista>
+          );
+        })}
+      </Box>
+    </Box>
+  );
+};
