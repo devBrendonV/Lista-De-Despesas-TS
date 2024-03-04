5e36eab61d9472563c945385477f87af68ab28a5
Brendon Victor <devbrendonvs@gmail.com>
2024-03-04 18:40:09 -0300

    Components/NovaTransacao/EntradaDeValores função do input do valor alterada

--- src/Components/NovaTransacao/EntradaDeValores.tsx
+++ src/Components/NovaTransacao/EntradaDeValores.tsx
@@ -1,83 +1,75 @@
 import FormControl from "@mui/material/FormControl";
 import { TextField } from "@mui/material";
 type Props = {
-    value:{
-        valor:number,
-        texto:string
-    },
-    adicionar:(valor:number,texto:string)=>void,
-    mudarValor:(valor:number)=>void
-    mudarTexto:(texto:string)=>void  
-}
+  value: {
+    valor: number;
+    texto: string;
+  };
+  adicionar: (valor: number, texto: string) => void;
+  mudarValor: (valor: number) => void;
+  mudarTexto: (texto: string) => void;
+};
 
 export const EntradaDeValores = (props: Props) => {
-    const max = 100000000;
-    const min = 0;
-    const {valor,texto} = props.value
-    const {adicionar,mudarValor,mudarTexto} = props
-    return (
-      <FormControl sx={{ display: "flex", width: "100%", paddingBottom: "25px" }}>
-        <TextField
-          variant="standard"
-          sx={{
-            marginBottom: "10px",
-            padding: "5px",
-          }}
-          inputProps={{
-            maxLength: 15,
-          }}
-          value={texto}
-          label="Digite o nome da transação:"
-          type="text"
-          onChange={(e) => mudarTexto(e.target.value.trim())}
-          onKeyDown={(e) => {
-            if (
-              texto !== "" &&
-              valor > 0
-            ) {
-              if (e.key === "Enter") {
-                adicionar(valor, texto);
-                mudarValor(0);
-                mudarTexto("");
-              }
+  const max = 100000000;
+  const min = 0;
+  const { valor, texto } = props.value;
+  const { adicionar, mudarValor, mudarTexto } = props;
+  return (
+    <FormControl sx={{ display: "flex", width: "100%", paddingBottom: "25px" }}>
+      <TextField
+        variant="standard"
+        sx={{
+          marginBottom: "10px",
+          padding: "5px",
+        }}
+        inputProps={{
+          maxLength: 15,
+        }}
+        value={texto}
+        label="Digite o nome da transação:"
+        type="text"
+        onChange={(e) => mudarTexto(e.target.value.trim())}
+        onKeyDown={(e) => {
+          if (texto !== "" && valor > 0) {
+            if (e.key === "Enter") {
+              adicionar(valor, texto);
+              mudarValor(0);
+              mudarTexto("");
             }
-          }}
-        />
-  
-        <TextField
-          variant="standard"
-          sx={{ padding: "5px" }}
-          value={valor}
-          label="Digite o valor da transação:"
-          type="number"
-          onKeyDown={(e) => {
-            if (
-              texto !== "" &&
-              valor > 0 
-            ) {
-              if (e.key === "Enter") {
-                adicionar(valor, texto);
-                mudarValor(0);
-                mudarTexto("");
-              }
+          }
+        }}
+      />
+
+      <TextField
+        onFocus={(e) => e.target.select()}
+        variant="standard"
+        sx={{ padding: "5px" }}
+        value={valor}
+        label="Digite o valor da transação:"
+        type="number"
+        onKeyDown={(e) => {
+          if (texto !== "" && valor > 0) {
+            if (e.key === "Enter") {
+              adicionar(valor, texto);
+              mudarValor(0);
+              mudarTexto("");
             }
-          }}
-          onChange={(e) => {
-            let b = Number(e.target.value)
-            if (b > 0) {
-              mudarValor(
-                parseFloat(Math.sqrt(b ** 2).toFixed(2))
-              );
-            }
-            if (b > max) {
-              mudarValor(max);
-            }
-            if (b <= min) {
-              mudarValor(min);
-            }
-          }}
-        />
-      </FormControl>
-    );
-  }
-  
+          }
+        }}
+        onChange={(e) => {
+          const valorInput = Number(e.target.value);
+          if (valorInput > 0) {
+            return mudarValor(parseFloat(valorInput.toFixed(2)));
+          }
+          if (valorInput > max) {
+            return mudarValor(max);
+          }
+          if (valorInput <= min) {
+            return mudarValor(min);
+          }
+        }}
+      />
+    </FormControl>
+  );
+};
