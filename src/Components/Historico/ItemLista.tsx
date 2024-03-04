3a07b3d540f5bbddc4717eb06908549ea2042885
Brendon Victor <devbrendonvs@gmail.com>
2024-03-04 11:43:36 -0300

    Components/Historico/ItemLista configurado

--- src/Components/Historico/ItemLista.tsx
+++ src/Components/Historico/ItemLista.tsx
@@ -1,9 +1,68 @@
-import React from 'react'
+import { useState, useContext } from "react";
+import { Box, Typography,IconButton } from "@mui/material";
+import { Context } from "../../Context";
+import DeleteIcon from "@mui/icons-material/Delete";
+import {FormatoLista} from "../../types/FormatoLista"
 
-type Props = {}
+
+type Props = {
+value:{
+    transacao: string,
+    tipo: boolean,
+    valor: number,
+    indice: number,
+    conteudo: FormatoLista,
+}
+}
 
 export const ItemLista = (props: Props) => {
-  return (
-    <div>ItemLista</div>
-  )
-}
+    const [opcao, setOpcao] = useState<boolean>(false);
+    const {excluirTransacao } = useContext(Context);
+    const data = props.value;
+    return (
+      <Box
+      sx={{
+        backgroundColor:"#faf7f7df"
+
+      }}
+        boxSizing={"border-box"}
+        boxShadow={5}
+        flexDirection={"row"}
+        marginBottom={"5px"}
+        minHeight={"55px"}
+        width={"100%"}
+        onMouseLeave={() => setOpcao(false)}
+        onMouseEnter={() => setOpcao(true)}
+        display={"flex"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+        borderRight={`7px solid ${data.tipo == true ? "#08f32fdf" : "#f00b0bed"}`}
+        title={`${data.tipo == true ? "+" : "-"} R$${data.valor}`}
+      >
+        <Typography
+          display={"flex"}
+          alignItems={"center"}
+          width={"50%"}
+          paddingLeft={"15px"}
+        >
+          {data.transacao}
+        </Typography>
+  
+        <Box
+          display={`${opcao == true ? "flex" : "none"}`}
+          flexDirection={"row"}
+          justifyContent={"flex-end"}
+          marginRight={"10px"}
+          width={"50%"}
+        >
+          <IconButton
+            title="Delete"
+            onClick={() => excluirTransacao(data.conteudo, data.indice)}
+          >
+            <DeleteIcon sx={{ fontSize: 25 , color:"#c91111"}} />
+          </IconButton>
+  
+        </Box>
+      </Box>
+    );
+  };
