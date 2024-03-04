c7f8c59dc8707e64e58f3d2af50538de02365202
Brendon Victor <devbrendonvs@gmail.com>
2024-03-04 11:53:46 -0300

    Components/NovaTransacao Components/NovaTransacao/OpcoesDeTransacao Configurado

--- src/Components/NovaTransacao/OpcoesDeTransacao.tsx
+++ src/Components/NovaTransacao/OpcoesDeTransacao.tsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import { useContext } from "react"
+import { Context } from "../../Context"
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 
-type Props = {}
-
-const OpcoesDeTransacao = (props: Props) => {
-  return (
-    <div>OpcoesDeTransacao</div>
-  )
-}
-
-export default OpcoesDeTransacao
+export const OpcoesDeTransacao = () => {
+    const {alterarTipoDeTransacao} = useContext(Context
+    )
+    return (
+        <FormControl
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            alignItems: "center",
+            justifyContent: "space-around",
+            width: "100%",
+            padding: "5px 0",
+          }}
+        >
+          <FormLabel id="demo-radio-buttons-group-label">
+            Tipo de transação:
+          </FormLabel>
+          <RadioGroup
+            row
+            aria-labelledby="demo-radio-buttons-group-label"
+            defaultValue="entrada"
+            name="radio-buttons-group"
+          >
+            <FormControlLabel
+              value="entrada"
+              control={<Radio onChange={() => alterarTipoDeTransacao(true)} />}
+              label="Entrada"
+            />
+            <FormControlLabel
+              value="saida"
+              control={<Radio onChange={() => alterarTipoDeTransacao(false)} />}
+              label="Saída"
+            />
+          </RadioGroup>
+        </FormControl>
+      );
+    }
--- src/Components/NovaTransacao/index.tsx
+++ src/Components/NovaTransacao/index.tsx
@@ -1,7 +1,49 @@
-import React from 'react'
+import { useContext, useState } from "react";
+import { Context } from "../../Context";
+import { Box, Button } from "@mui/material";
+import {OpcoesDeTransacao} from "./OpcoesDeTransacao";
+import AddIcon from "@mui/icons-material/Add";
 
-export const NovaTransacao = () => {
+type Props = {};
+
+export const NovaTransacao = (props: Props) => {
+  const { adicionar,tipoTransacao } = useContext(Context);
+
+  const [texto, setTexto] = useState("");
+  const [valor, setValor] = useState("");
+
   return (
-    <div>NovaTransacao</div>
-  )
-}
+    <Box
+      display={"flex"}
+      flexDirection={"column"}
+      height={"230px"}
+      color={"black"}
+    >
+      <OpcoesDeTransacao />
+
+      <Button
+        startIcon={<AddIcon />}
+        style={{
+          color: "#180202",
+          backgroundColor: `${
+            texto !== "" && valor !== 0 && valor !== ""
+              ? tipoTransacao
+                ? "#0de429"
+                : "#f70d0dd5"
+              : "#857e7ed5"
+          }`,
+          padding: "10px",
+        }}
+        onClick={() => {
+          if (texto !== "" && valor !== 0 && valor !== "") {
+            adicionar(valor, texto);
+            setValor("");
+            setTexto("");
+          }
+        }}
+      >
+        ADICIONAR
+      </Button>
+    </Box>
+  );
+};
